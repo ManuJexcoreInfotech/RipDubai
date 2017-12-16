0a730e3e69f7790ee35a475ae3c109d4cfea9493
Manu jexcore <web_dev_2@shubantecgnologies.com>
2017-12-16 12:57:24 +0530

    update

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,4 +1,4 @@
-import { Component,Pipe, PipeTransform } from '@angular/core';
+import { Component,Pipe, PipeTransform,ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 import { DatePicker } from '@ionic-native/date-picker';
@@ -16,20 +16,34 @@ import {InAppBrowser, InAppBrowserOptions} from '@ionic-native/in-app-browser';
   selector: 'page-payment',
   templateUrl: 'payment.html',
 })
+
+@ViewChild('iframe') iframe:ElementRef;
+
+
+
+
+
 export class PaymentPage {
 		
 	bookId=28;
 	url:SafeResourceUrl='';
 	constructor(public navCtrl: NavController, public navParams: NavParams, public constant:Constant, public service:WebService, public http: Http, public iab: InAppBrowser,public sanitizer: DomSanitizer) {
-		
+		this.constant.LoadingPresent();
 		this.bookId = this.navParams.get('book_id');
-		this.url = 'http://pr.veba.co/~shubantech/ripdubai/web/pm/checkout.php?book_id='+this.bookId ;
+		this.url = 'http://pr.veba.co/~shubantech/ripdubai/web/pm/checkout.php?book_id='+this.bookId;
 			
 	}
 	BackButtonClick(){
 		this.navCtrl.pop();
 	}
 	sanitize(url:string){   
-		return this.sanitizer.bypassSecurityTrustUrl(url);
+		return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+	}
+	dismissLoading(){
+		this.constant.LoadingHide();
+		console.log('me');
+		let doc =  this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentWindow;
+		var h = doc.getElementsByTagName('a');
+		console.log(this.iframe.nativeElement.contentDocument.document);
 	}
 }
